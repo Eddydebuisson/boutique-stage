3a1a3dd1ba25afb8125546a0fbc010d9eb00e226
RYMO-amaury <tcom.amaury@gmail.com>
2022-06-29 12:37:37 +0200

    css accueil

--- src/component/Top/TopItem.js
+++ src/component/Top/TopItem.js
@@ -1,25 +1,27 @@
 import './TopItem.css'
 import lotus from '../../assets/lotus.png'
 
-const topItem = (props) =>{
+const topItem = (props) =>
+{
 
-return (
-    <>
-        <div className="top-item">
-            <div className="top-info">
-            <img src={lotus}></img>
-            <div className="title">{props.title}</div>
-            <img src={lotus}></img>
+    return (
+        <>
+            <div className="top-item">
+                <div className="top-info">
+                    <img src={lotus} className="lotus"></img>
+                    <div className="title">{props.title}</div>
+                    <img src={lotus} className="lotus"></img>
+                </div>
+                <div className="img-shop">
+                    <img src={props.img}></img>
+                    <img src={props.img}></img>
+                    <img src={props.img}></img>
+                </div>
+                <div className='br'></div>
             </div>
-            <div className="img-shop">
-            <img src={props.img}></img>
-            <img src={props.img}></img>
-            <img src={props.img}></img>
-            </div>
-        </div>
-    
-    </>
-)
+
+        </>
+    )
 
 }
 
