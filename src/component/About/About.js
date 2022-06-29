33270a24f0da1b4e84600478e45829f69bf746e9
RYMO-amaury <tcom.amaury@gmail.com>
2022-06-29 16:03:58 +0200

    about

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -9,9 +9,10 @@ const About = () =>
                 <div className='hr'></div>
                 <div className='header'>
                     <div className='about'>
-                        <h2 className=''>A Propos</h2>
+                        {/* <h2 className=''>A Propos</h2>
                         <p>Originaire des Hauts de France, j'ai pris la route pour le sud, puis j'ai posé mes valises en Ardèche  «  Merveillous Païs »
-                            Tout a commencé dans le but de faire plaisir à mes proches  pour des événements ( anniversaires, fête des mères, Noël...). </p>
+                            Tout a commencé dans le but de faire plaisir à mes proches  pour des événements ( anniversaires, fête des mères, Noël...). </p> */
+                        }
                     </div>
                 </div>
             </section>
