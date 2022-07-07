901922ce09375338de6fe3b5a990c56d806ae147
Eddydebuisson <eddydebuisson@gmail.com>
2022-07-07 12:56:48 +0200

    ajout de la liste des produits

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Accueil from './pages/Accueil'
+import Accueil from './pages/Accueil/Accueil'
 import Register from './component/Register/Register'
 import Accessoire from './pages/Accessoire';
 import Ecoresponsable from './pages/Ecoresponsable';
@@ -8,6 +8,7 @@ import Enfant from './pages/Enfant';
 import Panier from './pages/Panier';
 import Pull from './pages/Pull';
 import Tshirt from './pages/Tshirt';
+import Product from './pages/Product/Product';
 
 function App()
 {
@@ -15,6 +16,7 @@ function App()
     <>
       <Router>
         <Routes>
+          
           <Route path='/' element={<Accueil />}></Route>
           <Route path='/Accessoire' element={<Accessoire />}></Route>
           <Route path='/Ecoresponsable' element={<Ecoresponsable />}></Route>
@@ -22,6 +24,7 @@ function App()
           <Route path='/Panier' element={<Panier />}></Route>
           <Route path='/Pull' element={<Pull />}></Route>
           <Route path='/Tshirt' element={<Tshirt />}></Route>
+          <Route path='/produits' element={<Product />}></Route>
         </Routes>
       </Router>
     </>
